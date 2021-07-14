6e4b0b5c679c119e4def761cd0af86eea34febf7
Jeremy Munshower <80923758+Muneshyne@users.noreply.github.com>
2021-07-14 14:32:14 -0700

    Add files via upload

--- PkgExpress/PkgExpress/Program.cs
+++ PkgExpress/PkgExpress/Program.cs
@@ -24,7 +24,7 @@ namespace PkgExpress
             }
             else if (pkgWeight <= 50)
             {
-                Console.WriteLine("WIDTH");
+                Console.WriteLine("Size");
             }
             //string result = pkgWeight <= 50 ? "Please continue to next step." : "Package to heavy to be shipped via Package Express. Have a good day.";
             //Console.WriteLine(result);
@@ -34,43 +34,25 @@ namespace PkgExpress
             Console.WriteLine("What is the package width?");
             int pkgWidth = Convert.ToInt32(Console.ReadLine());
 
-            if (pkgWidth > 50)
-            {
-                Console.WriteLine("Package too big to be shipped via Package Express.");
-                Console.ReadLine();
-                Environment.Exit(-1);
-            }
-            else if (pkgWidth <= 50)
-            {
-                Console.WriteLine("HEIGHT");
-            }
+           
 
             // HEIGHT
             Console.WriteLine("What is the height of the package?");
             int pkgHeight = Convert.ToInt32(Console.ReadLine());
 
-            if (pkgHeight > 50)
-            {
-                Console.WriteLine("Package too big to be shipped via Package Express.");
-                Console.ReadLine();
-                Environment.Exit(-1);
-            }
-            else if (pkgHeight <= 50)
-            {
-                Console.WriteLine("LENGTH");
-            }
+            
 
             // LENGTH
             Console.WriteLine("What is the packages length?");
             int pkgLength = Convert.ToInt32(Console.ReadLine());
 
-            if (pkgLength > 50)
+            if (pkgLength + pkgHeight + pkgWidth > 50)
             {
                 Console.WriteLine("Package too big to be shipped via Package Express.");
                 Console.ReadLine();
                 Environment.Exit(-1);
             }
-            else if (pkgLength <= 50)
+            else if (pkgLength + pkgHeight + pkgWidth <= 50)
             {
                 Console.WriteLine("Calculating shipping cost...");
             }
