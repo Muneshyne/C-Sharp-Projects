f94f807285d14ef10bea6d0cb72c3337a9064c48
Jeremy Munshower <80923758+Muneshyne@users.noreply.github.com>
2021-07-15 08:47:25 -0700

    Add files via upload

---
+++ LoopAssignment/LoopAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string input = Console.ReadLine();
+
+            //while (!input.Equals(string.Empty))
+            //{
+            //    Console.WriteLine("Input is {0}.", input);
+            //    input = Console.ReadLine();
+            //}
+
+            string input2 = string.Empty;
+            do
+            {
+
+                input2 = Console.ReadLine();
+                Console.WriteLine("Input is {0}.", input2);
+
+            } while (!input2.Equals(string.Empty));
+        }
+    }
+}
