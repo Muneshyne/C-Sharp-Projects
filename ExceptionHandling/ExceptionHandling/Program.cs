cbd5e02a392b8adbfaf846dcbe4e8ffbf5155670
Jeremy Munshower <jdmunshower@gmail.com>
2021-07-16 20:56:21 -0700

    submisions

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace ExceptionHandling // to catch errors (try / catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number.");
+                int numOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number.");
+                int numTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two...");
+                int numThree = numOne / numTwo;
+                Console.WriteLine(numOne + " divided by " + numTwo + " equals " + numThree);
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine(); // a lot of the time you'll have a dB log here in the finally statement
+            }
+            
+        }
+    }
+}
---
+++ StringsAndIntegersAssignment/StringsAndIntegersAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace StringsAndIntegersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a list of integers
+            List<int> numList = new List<int> { 6, 84, 45, 7624, 528, 122 };
+
+            try
+            {
+                //Ask the user for a number to divide each number in the list by
+                Console.WriteLine("\tEnter a number you wish to divide by in the list. ");
+                
+                int quotient = Convert.ToInt32(Console.ReadLine());
+                // Write a loop that takes each integer in the list,
+                // divides it by the number the user entered, and displays the result to the screen
+                foreach (int num in numList)
+                {
+                    Console.WriteLine(num / quotient);
+                }
+            }
+            catch (DivideByZeroException ex) //no dividing by 0
+            {
+                Console.WriteLine("\tPlease do not divide by ZERO.");
+            }
+            // to catch non integar entered. Input string was not in a correct format.
+            catch (FormatException ex)
+            {
+                Console.WriteLine("\tPlease enter a whole number.");
+            }
+            catch (Exception ex) //a general error message if any wrong data type entered.
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            Console.WriteLine("The program emerged from try/catch block and continued with exectution.");
+            Console.Read();
+        }
+    }
+}
