b7f586aefc25e9f62b3a9b89694f445f215f20bc
Jeremy Munshower <jdmunshower@gmail.com>
2021-07-18 19:54:31 -0700

    .

---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate the class.
+            Void class1 = new Void();
+            // call the method in the class, passing in two numbers. 
+            class1.method(34, 248);
+            // call the method in the class, specifying the parameters by name. 
+            class1.method(num1: 23, num2: 13);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Void.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Void
+    {
+        // create a void method that takes two integers as parameters
+        public void method(int num1, int num2)
+        {
+            // have the method do a math operation on the first integer and display the second integer to the screen.
+            num1 += num1;
+            Console.WriteLine(num2);
+        }
+    }
+}
