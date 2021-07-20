afac65041c445e2380da492267393b09654ffc52
Jeremy Munshower <jdmunshower@gmail.com>
2021-07-20 16:22:54 -0700

    assignment

---
+++ AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate an Employee object with the firstName "Sample" and lastName "Student"
+            Employee employee = new Employee()
+            {
+                firstName = "Sample",
+                lastName = "Student"
+            };
+            // call the SayName() method on the object. 
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
--- TwentyOne/TwentyOne/Game.cs
+++ TwentyOne/TwentyOne/Game.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 
 namespace TwentyOne
 {
-    public class Game //keep names generic ie not TwentyOneGame
+    public abstract class Game //keep names generic ie not TwentyOneGame
     {
         public List<string> Players { get; set; }
         public string Name { get; set; }
         public string Dealer { get; set; }
         // can type prop then tab x2 to set public property
-        public void ListPlayers()
+        public abstract void Play(); //no implementation
+
+        public virtual void ListPlayers() // does have implementation
         {
             foreach (string player in Players)
             {
