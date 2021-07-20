b83ee1b1fe776effc07ffc42bfd04f03d01b33d3
Jeremy Munshower <jdmunshower@gmail.com>
2021-07-20 13:16:23 -0700

    added static class

---
+++ TwentyOne/TwentyOne/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Game //keep names generic ie not TwentyOneGame
+    {
+        public List<string> Players { get; set; }
+        public string Name { get; set; }
+        public string Dealer { get; set; }
+        // can type prop then tab x2 to set public property
+        public void ListPlaers()
+        {
+            foreach (string player in Players)
+            {
+                Console.WriteLine(player);
+            }
+        }
+    }
+}
